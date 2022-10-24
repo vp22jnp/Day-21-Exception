87541690e102979bf3cdc1a3f149fcb5403f090c
vp22j <vp22jnp@gmail.com>
2022-10-24 19:37:01 +0530

    added: firstname validation using exception

---
+++ main/java/com/UserRegistration/FirstName.java
@@ -0,0 +1,27 @@
+package com.UserRegistration;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+public class FirstName extends Exception {
+    public static void main(String[] args) throws InvalidInputByUser {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first Name");
+        String firstName = sc.next();
+        FirstName userRegistrationProblem = new FirstName();
+        userRegistrationProblem.firstNameCheck(firstName);
+    }
+    public boolean firstNameCheck(String name){
+        Pattern pattern1 = Pattern.compile("[A-Z]{1}[a-z]{3,}");
+        Matcher matcher1 = pattern1.matcher(name);
+        if (matcher1.matches()) {
+            System.out.println("Valid First Name");
+            return true;
+        } else System.out.println("Not valid");
+        return false;
+    }
+
+}
+
+
